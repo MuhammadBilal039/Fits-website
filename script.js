a7129d4b4b06922461b9553142ba4dbbce2c5250
Muhammad Bilal <muhammad.bilal2021pk@gmail.com>
2025-12-22 19:43:10 +0500

    Service card section added and make responsive website

--- script.js
+++ script.js
@@ -1,26 +1,65 @@
-// MEGA DROPDOWN OPEN/CLOSE
-const servicesBtn = document.getElementById("servicesBtn");
-const servicesDropdown = document.getElementById("servicesDropdown");
+// // MEGA DROPDOWN OPEN/CLOSE
+// const servicesBtn = document.getElementById("servicesBtn");
+// const servicesDropdown = document.getElementById("servicesDropdown");
 
-servicesBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  servicesDropdown.style.display =
-    servicesDropdown.style.display === "block" ? "none" : "block";
+// servicesBtn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   servicesDropdown.style.display =
+//     servicesDropdown.style.display === "block" ? "none" : "block";
+// });
+
+// // MOBILE MENU TOGGLE
+// const mobileMenuBtn = document.getElementById("mobileMenuBtn");
+// const navLinks = document.querySelector(".nav-links");
+
+// mobileMenuBtn.addEventListener("click", () => {
+//   navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
+// });
+
+// const track = document.getElementById("track");
+// const cards = Array.from(track.children);
+
+// // Duplicate cards
+// cards.forEach((card) => {
+//   const clone = card.cloneNode(true);
+//   track.appendChild(clone);
+// });
+
+const menuBtn = document.getElementById("menu");
+const navLinks = document.getElementById("navLinks");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+
+  // icon change
+  if (navLinks.classList.contains("active")) {
+    menuBtn.innerHTML = "✖";
+  } else {
+    menuBtn.innerHTML = "☰";
+  }
 });
 
-// MOBILE MENU TOGGLE
-const mobileMenuBtn = document.getElementById("mobileMenuBtn");
-const navLinks = document.querySelector(".nav-links");
+// Carousel Card Slider
+const carouselTrack = document.getElementById("carouselTrack");
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
 
-mobileMenuBtn.addEventListener("click", () => {
-  navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
+const cardWidth = 344; // 320 + 24 gap
+const totalCards = document.querySelectorAll(".service-card").length;
+const visibleCards = 3;
+
+let index = 0;
+
+nextBtn.addEventListener("click", () => {
+  if (index < totalCards - visibleCards) {
+    index++;
+    carouselTrack.style.transform = `translateX(-${index * cardWidth}px)`;
+  }
 });
 
-const track = document.getElementById("track");
-const cards = Array.from(track.children);
-
-// Duplicate cards
-cards.forEach((card) => {
-  const clone = card.cloneNode(true);
-  track.appendChild(clone);
+prevBtn.addEventListener("click", () => {
+  if (index > 0) {
+    index--;
+    carouselTrack.style.transform = `translateX(-${index * cardWidth}px)`;
+  }
 });
